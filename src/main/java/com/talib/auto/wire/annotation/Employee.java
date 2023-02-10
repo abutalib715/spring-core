841ab75a5e7a358d389f22853916e153ec434ff3
Talib <talib.superbnexus@gmail.com>
2023-02-10 13:01:54 +0600

    autowire qualifier

--- src/main/java/com/talib/auto/wire/annotation/Employee.java
+++ src/main/java/com/talib/auto/wire/annotation/Employee.java
@@ -1,23 +1,23 @@
 package com.talib.auto.wire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
 
-
+	@Autowired
+	@Qualifier("address2")
 	private Address address;
 
 	public Address getAddress() {
 		return address;
 	}
 
-	@Autowired
 	public void setAddress(Address address) {
 		System.out.println("Setting value");
 		this.address = address;
 	}
 
-	@Autowired
 	public Employee(Address address) {
 		System.out.println("Setting value via constructor");
 		this.address = address;
