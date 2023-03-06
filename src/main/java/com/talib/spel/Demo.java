ab633c6735893af90ed9f3fcff1a6f1cd8c456a9
Talib <talib.superbnexus@gmail.com>
2023-03-06 23:43:57 +0600

    SpEL static method, variable, boolean type

--- src/main/java/com/talib/spel/Demo.java
+++ src/main/java/com/talib/spel/Demo.java
@@ -9,6 +9,25 @@ public class Demo {
 	@Value("#{12+32}")
 	private int x;
 	private int y;
+	
+	@Value("#{ T(java.lang.Math).sqrt(25) }")
+	private int z;
+	
+	@Value("#{ T(java.lang.Math).PI }")
+	private Double pi;
+
+	@Value("#{ new java.lang.String('talib') }")
+	private String name;
+	
+	@Value("#{ 6>3 }")
+	private Boolean isActive;
+	
+	public int getZ() {
+		return z;
+	}
+	public void setZ(int z) {
+		this.z = z;
+	}
 	public int getX() {
 		return x;
 	}
@@ -21,10 +40,25 @@ public class Demo {
 	public void setY(int y) {
 		this.y = y;
 	}
+	public Double getPi() {
+		return pi;
+	}
+	public void setPi(Double pi) {
+		this.pi = pi;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", pi=" + pi + ", name=" + name + ", isActive=" + isActive
+				+ "]";
 	}
+	
+	
 	
 	
 }
